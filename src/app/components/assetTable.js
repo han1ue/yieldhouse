00d247f1c7b79b9b0cfe03c3e566cbf81845f7ab
jvalente <notfound404token@gmail.com>
2024-05-22 20:29:33 +0100

    add pagination

--- src/app/components/assetTable.js
+++ src/app/components/assetTable.js
@@ -11,6 +11,7 @@ import {
   Box,
 } from "@radix-ui/themes";
 import moment from "moment";
+import Pagination from "@mui/material/Pagination";
 import Image from "next/image";
 import { ThickArrowUpIcon, ThickArrowDownIcon } from "@radix-ui/react-icons";
 import "react-circular-progressbar/dist/styles.css";
@@ -25,6 +26,8 @@ export default function AssetTable({
 }) {
   const [yieldsData, setYieldsData] = useState([]);
   const [tableData, setTableData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
   const testnet = useTestnetContext();
   const [sortConfig, setSortConfig] = useState({
     key: null,
@@ -79,6 +82,10 @@ export default function AssetTable({
 
     // Sort data if sortConfig is set
   }, [selectedChains, selectedAssetTypes, searchQuery, yieldsData]);
+
+  const handlePageChange = (event, value) => {
+    setCurrentPage(value);
+  };
 
   function sortData(data, key, direction) {
     setSortConfig({ key, direction });
@@ -153,112 +160,141 @@ export default function AssetTable({
   };
 
   return (
-    <Table.Root variant="ghost" size="1">
-      <Table.Header>
-        <Table.Row>
-          <Table.ColumnHeaderCell>
-            <Flex mt="1">
-              <Text size="2">Asset</Text>
-            </Flex>
-          </Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell>
-            <Flex direction="row" align="center" gap="1">
-              <Text size="2">APY</Text>
-              <Box>{renderSortIcons("apy")}</Box>
-            </Flex>
-          </Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell>
-            <Flex direction="row" align="center" gap="1">
-              <Box>TVL</Box>
-              <Box>{renderSortIcons("tvl")}</Box>
-            </Flex>
-          </Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell>
-            <Flex mt="1">
-              <Text size="2">Type</Text>
-            </Flex>
-          </Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell>
-            <Flex mt="1">
-              <Text size="2">Risk</Text>
-            </Flex>
-          </Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
-        </Table.Row>
-      </Table.Header>
+    <Flex direction="column" gap="6" align="center">
+      <Box width="100%">
+        <Table.Root variant="ghost" size="1">
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeaderCell>
+                <Flex mt="1">
+                  <Text size="2">Asset</Text>
+                </Flex>
+              </Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>
+                <Flex direction="row" align="center" gap="1">
+                  <Text size="2">APY</Text>
+                  <Box>{renderSortIcons("apy")}</Box>
+                </Flex>
+              </Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>
+                <Flex direction="row" align="center" gap="1">
+                  <Box>TVL</Box>
+                  <Box>{renderSortIcons("tvl")}</Box>
+                </Flex>
+              </Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>
+                <Flex mt="1">
+                  <Text size="2">Type</Text>
+                </Flex>
+              </Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>
+                <Flex mt="1">
+                  <Text size="2">Risk</Text>
+                </Flex>
+              </Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
+            </Table.Row>
+          </Table.Header>
 
-      <Table.Body>
-        {tableData.map((row, index) => (
-          <Table.Row key={index} align="center">
-            <Table.RowHeaderCell>
-              <Flex direction="row" align="center">
-                <Box>
-                  <Image
-                    src={
-                      "/images/assets/" + row.asset.name.toLowerCase() + ".svg"
-                    }
-                    width={24}
-                    height={24}
-                  />
-                </Box>
-                <Flex direction="row" align="start">
-                  <Text size="3" weight="medium" ml="2" trim="start">
-                    {row.asset.name}
-                  </Text>
-                  {row.type == "LP" && (
-                    <Text size="1" color="gray" ml="1">
-                      {"/ " + row.baseAsset}
+          <Table.Body>
+            {tableData
+              .slice(
+                (currentPage - 1) * itemsPerPage,
+                currentPage * itemsPerPage
+              )
+              .map((row, index) => (
+                <Table.Row key={index} align="center">
+                  <Table.RowHeaderCell>
+                    <Flex direction="row" align="center">
+                      <Box>
+                        <Image
+                          src={
+                            "/images/assets/" +
+                            row.asset.name.toLowerCase() +
+                            ".svg"
+                          }
+                          width={24}
+                          height={24}
+                        />
+                      </Box>
+                      <Flex direction="row" align="start">
+                        <Text size="3" weight="medium" ml="2" trim="start">
+                          {row.asset.name}
+                        </Text>
+                        {row.type == "LP" && (
+                          <Text size="1" color="gray" ml="1">
+                            {"/ " + row.baseAsset}
+                          </Text>
+                        )}
+                        {row.apy.type == "fixed" && (
+                          <Text size="1" color="gray" ml="1">
+                            {"(" + formatTime(row.apy.maturityTimestamp) + ")"}
+                          </Text>
+                        )}
+                      </Flex>
+                    </Flex>
+                    <Text size="1" weight="light">
+                      {row.protocol + " • " + row.chain.name}{" "}
                     </Text>
-                  )}
-                  {row.apy.type == "fixed" && (
-                    <Text size="1" color="gray" ml="1">
-                      {"(" + formatTime(row.apy.maturityTimestamp) + ")"}
-                    </Text>
-                  )}
-                </Flex>
-              </Flex>
-              <Text size="1" weight="light">
-                {row.protocol + " • " + row.chain.name}{" "}
-              </Text>
-            </Table.RowHeaderCell>
-            <Table.Cell>
-              <Flex direction="column" gap="1">
-                <Flex>{Number(row.apy.value * 100).toFixed(2) + "%"}</Flex>
-                <Flex>
-                  <Badge variant="soft" color="blue" size="1" radius="large">
-                    {row.apy.type}
-                  </Badge>
-                </Flex>
-              </Flex>
-            </Table.Cell>
-            <Table.Cell>
-              {Intl.NumberFormat("en-US", {
-                notation: "compact",
-                maximumFractionDigits: 2,
-              }).format(row.tvl) + " $"}
-            </Table.Cell>
-            <Table.Cell>
-              <Flex direction="column" gap="1" display="inline-flex">
-                {row.type.map((type, i) => (
-                  <Badge key={i} variant="soft" color="iris" radius="large">
-                    {type}
-                  </Badge>
-                ))}
-              </Flex>
-            </Table.Cell>
-            <Table.Cell>
-              <RiskIndicator risk={row.risk} size={32} textSize={48} />
-            </Table.Cell>
-            <Table.Cell>
-              <Flex direction="row" width="90%" justify="end">
-                <Link href={"/details/" + tableData[index].id}>
-                  <Button variant="soft">Deposit</Button>
-                </Link>
-              </Flex>
-            </Table.Cell>
-          </Table.Row>
-        ))}
-      </Table.Body>
-    </Table.Root>
+                  </Table.RowHeaderCell>
+                  <Table.Cell>
+                    <Flex direction="column" gap="1">
+                      <Flex>
+                        {Number(row.apy.value * 100).toFixed(2) + "%"}
+                      </Flex>
+                      <Flex>
+                        <Badge
+                          variant="soft"
+                          color="blue"
+                          size="1"
+                          radius="large"
+                        >
+                          {row.apy.type}
+                        </Badge>
+                      </Flex>
+                    </Flex>
+                  </Table.Cell>
+                  <Table.Cell>
+                    {Intl.NumberFormat("en-US", {
+                      notation: "compact",
+                      maximumFractionDigits: 2,
+                    }).format(row.tvl) + " $"}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Flex direction="column" gap="1" display="inline-flex">
+                      {row.type.map((type, i) => (
+                        <Badge
+                          key={i}
+                          variant="soft"
+                          color="iris"
+                          radius="large"
+                        >
+                          {type}
+                        </Badge>
+                      ))}
+                    </Flex>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <RiskIndicator risk={row.risk} size={32} textSize={48} />
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Flex direction="row" width="90%" justify="end">
+                      <Link href={"/details/" + tableData[index].id}>
+                        <Button variant="soft">Deposit</Button>
+                      </Link>
+                    </Flex>
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+          </Table.Body>
+        </Table.Root>
+      </Box>
+      <Pagination
+        count={Math.ceil(tableData.length / itemsPerPage)}
+        onChange={handlePageChange}
+        variant="outlined"
+        shape="rounded"
+      />
+    </Flex>
   );
 }
